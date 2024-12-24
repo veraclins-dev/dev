805a0ebfcc8ca1e9e438244bfc666c0c63c841ce
clinton.agada <agadaclinton@gmsil.com>
2024-12-24 23:40:15 +0100

    fix: use Icon from external icons package

--- packages/ui/src/lib/ui/icon.tsx
+++ packages/ui/src/lib/ui/icon.tsx
@@ -1,66 +1,64 @@
 import { type SVGProps } from 'react';
 
-import href from '@veraclins-dev/icons/sprite.svg';
-import { cn } from '@veraclins-dev/utils';
-
-import { type IconName } from '../../icons/name';
+import {
+  Icon as Component,
+  type IconName,
+  type IconProps as ComponentProps,
+} from '@veraclins-dev/icons';
 
 import { ComposedTooltip } from './tooltip';
 
-const sizeClassName = {
-  font: 'w-[1em] h-[1em]',
-  xs: 'w-4 h-4',
-  sm: 'w-5 h-5',
-  md: 'w-6 h-6',
-  lg: 'w-8 h-8',
-  xl: 'w-10 h-10',
-} as const;
+// const sizeClassName = {
+//   font: 'w-[1em] h-[1em]',
+//   xs: 'w-4 h-4',
+//   sm: 'w-5 h-5',
+//   md: 'w-6 h-6',
+//   lg: 'w-8 h-8',
+//   xl: 'w-10 h-10',
+// } as const;
 
-type Size = keyof typeof sizeClassName;
+// type Size = keyof typeof sizeClassName;
 
-const childrenSizeClassName = {
-  font: 'gap-1.5',
-  xs: 'gap-1.5',
-  sm: 'gap-1.5',
-  md: 'gap-2',
-  lg: 'gap-2',
-  xl: 'gap-3',
-} satisfies Record<Size, string>;
+// const childrenSizeClassName = {
+//   font: 'gap-1.5',
+//   xs: 'gap-1.5',
+//   sm: 'gap-1.5',
+//   md: 'gap-2',
+//   lg: 'gap-2',
+//   xl: 'gap-3',
+// } satisfies Record<Size, string>;
 
-type IconProps = SVGProps<SVGSVGElement> & {
-  name: IconName;
-  size?: Size;
-  href?: string;
+type IconProps = ComponentProps & {
   tooltip?: string;
 };
 
-function Component({
-  name,
-  size = 'font',
-  className,
-  children,
-  // href = href,
-  ...props
-}: Omit<IconProps, 'tooltip'>) {
-  if (children) {
-    return (
-      <span
-        className={`inline-flex items-center ${childrenSizeClassName[size]}`}
-      >
-        <Component name={name} size={size} className={className} {...props} />
-        {children}
-      </span>
-    );
-  }
-  return (
-    <svg
-      {...props}
-      className={cn(sizeClassName[size], 'inline self-center', className)}
-    >
-      <use href={`${href}#${name}`} />
-    </svg>
-  );
-}
+// function Component({
+//   name,
+//   size = 'font',
+//   className,
+//   children,
+//   // href = href,
+//   ...props
+// }: Omit<IconProps, 'tooltip'>) {
+//   if (children) {
+//     return (
+//       <span
+//         className={`inline-flex items-center ${childrenSizeClassName[size]}`}
+//       >
+//         <Component name={name} size={size} className={className} {...props} />
+//         {children}
+//       </span>
+//     );
+//   }
+//   return (
+//     <svg
+//       {...props}
+//       className={cn(sizeClassName[size], 'inline self-center', className)}
+//     >
+//       <use href={`${href}#${name}`} />
+//     </svg>
+//   );
+// }
 
 /**
  * Renders an SVG icon. The icon defaults to the size of the font. To make it
